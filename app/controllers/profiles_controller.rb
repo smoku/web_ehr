c0a03851b60e88ade7bf1cc926ae968edd1fa75e
Paweł Smoczyk <pawel@smoczyk.pl>
2011-04-29 13:16:49 +0200

    sync

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,28 +10,28 @@ class ProfilesController < ApplicationController
       profile_data[:birth_date] = profile.birth_date
       profile_data[:blood_type] = profile.blood_type
       profile_data[:gender] = profile.gender
-      # profile_data[:medications] = profile.medications.map do |medication|
-      #   medication_data = {}
-      #   medication_data[:id] = medication.id
-      #   medication_data[:name] = medication.name
-      #   medication_data[:started_date] = medication.started_date
-      #   medication_data[:ended_date] = medication.ended_date
-      #   medication_data[:strength] = medication.strength
-      #   medication_data[:dose] = medication.dose
-      #   medication_data[:frequency] = medication.frequency
-      #   medication_data[:note] = medication.note
-      #   medication_data
-      # end
-      # profile_data[:results] = profile.results.map do |result|
-      #   result_data = {}
-      #   result_data[:id] = result.id
-      #   result_data[:name] = result.name
-      #   result_data[:result] = result.result
-      #   result_data[:units] = result.units
-      #   result_data[:date] = result.date
-      #   result_data[:note] = result.note
-      #   result_data
-      # end
+      profile_data[:medications] = profile.medications.map do |medication|
+        medication_data = {}
+        medication_data[:id] = medication.id
+        medication_data[:name] = medication.name
+        medication_data[:started_date] = medication.started_date
+        medication_data[:ended_date] = medication.ended_date
+        medication_data[:strength] = medication.strength
+        medication_data[:dose] = medication.dose
+        medication_data[:frequency] = medication.frequency
+        medication_data[:note] = medication.note
+        medication_data
+      end
+      profile_data[:results] = profile.results.map do |result|
+        result_data = {}
+        result_data[:id] = result.id
+        result_data[:name] = result.name
+        result_data[:result] = result.result
+        result_data[:units] = result.units
+        result_data[:date] = result.date
+        result_data[:note] = result.note
+        result_data
+      end
       profile_data
     end
     
